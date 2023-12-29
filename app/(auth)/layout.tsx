461a0afed786bfee063cef049202b0e5f5c514bb
Kartik Arora <kartikaroraofficial01@gmail.com>
2023-12-30 02:54:24 +0530

    added auth and theme-provider

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AuthLayout = ({ children }:{ children: React.ReactNode }) => {
     return (
-        <div className='bg-red-500 h-full w-full'>
+        <div className='bg-slate-300 h-full w-full flex place-items-center justify-center'>
             {children}
         </div>
     )
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from '@clerk/nextjs'
 
 const Home = () => {
   return (
     <div>
       Discord Clone Page <br />
       <Button>Button</Button>
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
